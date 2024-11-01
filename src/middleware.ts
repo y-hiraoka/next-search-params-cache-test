0d8b6fda6409a31641815a06e83e6732766b284d
Yosuke Hiraoka <yosuke.h@chot-inc.com>
2024-11-01 14:04:38 +0900

    searchParamsを使ってmiddlewareでキャッシュコントロール

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export const middleware = async (): Promise<Response> => {
+  return NextResponse.next({
+    headers: {
+      "X-From-Middleware": "true",
+      "Cache-Control": `public, s-maxage=60, stale-while-revalidate=60`,
+    },
+  });
+};
