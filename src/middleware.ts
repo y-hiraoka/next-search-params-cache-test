aabc5b1f99b0011820d20cea2a87e47a7c2c04ac
Yosuke Hiraoka <yosuke.h@chot-inc.com>
2024-11-01 14:24:44 +0900

    swrやめてみる

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ export const middleware = async (): Promise<Response> => {
   return NextResponse.next({
     headers: {
       "X-From-Middleware": "true",
-      "Vercel-CDN-Cache-Control": `public, s-maxage=60, stale-while-revalidate=60`,
+      "Cache-Control": "public, max-age=60, must-revalidate",
     },
   });
 };
